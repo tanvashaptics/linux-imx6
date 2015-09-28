boundarydevices/linux

fec_main: use mac set by u-boot before fuses

drivers/net/ethernet/freescale/fec_main.c
@@ -1658,7 +1658,18 @@ static void fec_get_mac(struct net_device *ndev)
 	iap = macaddr;
 
 	/*
-	 * 2) from device tree data
+	 * 2) FEC mac registers set by bootloader
+	 */
+	if (!is_valid_ether_addr(iap)) {
+		*((unsigned long *) &tmpaddr[0]) =
+			be32_to_cpu(readl(fep->hwp + FEC_ADDR_LOW));
+		*((unsigned short *) &tmpaddr[4]) =
+			be16_to_cpu(readl(fep->hwp + FEC_ADDR_HIGH) >> 16);
+		iap = &tmpaddr[0];
+	}
+
+	/*
+	 * 3) from device tree data
 	 */
 	if (!is_valid_ether_addr(iap)) {
 		struct device_node *np = fep->pdev->dev.of_node;
@@ -1670,7 +1681,7 @@ static void fec_get_mac(struct net_device *ndev)
 	}
 
 	/*
-	 * 3) from flash or fuse (via platform data)
+	 * 4) from flash or fuse (via platform data)
 	 */
 	if (!is_valid_ether_addr(iap)) {
 #ifdef CONFIG_M5272
@@ -1680,17 +1691,6 @@ static void fec_get_mac(struct net_device *ndev)
 		if (pdata)
 			iap = (unsigned char *)&pdata->mac;
 #endif
-	}
-
-	/*
-	 * 4) FEC mac registers set by bootloader
-	 */
-	if (!is_valid_ether_addr(iap)) {
-		*((unsigned long *) &tmpaddr[0]) =
-			be32_to_cpu(readl(fep->hwp + FEC_ADDR_LOW));
-		*((unsigned short *) &tmpaddr[4]) =
-			be16_to_cpu(readl(fep->hwp + FEC_ADDR_HIGH) >> 16);
-		iap = &tmpaddr[0];
 	}
 
 	/*

tanvashaptics/linux-imx6

drivers/net/ethernet/freescale/fec_main.c
@@ -1683,7 +1683,18 @@ static void fec_get_mac(struct net_device *ndev)
 	iap = macaddr;
 
 	/*
-	 * 2) from device tree data
+	 * 2) FEC mac registers set by bootloader
+	 */
+	if (!is_valid_ether_addr(iap)) {
+		*((__be32 *) &tmpaddr[0]) =
+			cpu_to_be32(readl(fep->hwp + FEC_ADDR_LOW));
+		*((__be16 *) &tmpaddr[4]) =
+			cpu_to_be16(readl(fep->hwp + FEC_ADDR_HIGH) >> 16);
+		iap = &tmpaddr[0];
+	}
+
+	/*
+	 * 3) from device tree data
 	 */
 	if (!is_valid_ether_addr(iap)) {
 		struct device_node *np = fep->pdev->dev.of_node;
@@ -1695,7 +1706,7 @@ static void fec_get_mac(struct net_device *ndev)
 	}
 
 	/*
-	 * 3) from flash or fuse (via platform data)
+	 * 4) from flash or fuse (via platform data)
 	 */
 	if (!is_valid_ether_addr(iap)) {
 #ifdef CONFIG_M5272
@@ -1705,17 +1716,6 @@ static void fec_get_mac(struct net_device *ndev)
 		if (pdata)
 			iap = (unsigned char *)&pdata->mac;
 #endif
-	}
-
-	/*
-	 * 4) FEC mac registers set by bootloader
-	 */
-	if (!is_valid_ether_addr(iap)) {
-		*((__be32 *) &tmpaddr[0]) =
-			cpu_to_be32(readl(fep->hwp + FEC_ADDR_LOW));
-		*((__be16 *) &tmpaddr[4]) =
-			cpu_to_be16(readl(fep->hwp + FEC_ADDR_HIGH) >> 16);
-		iap = &tmpaddr[0];
 	}
 
 	/*